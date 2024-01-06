fb38f2361f6c732197c86864d6347d3737f5fdf0
Petko Todorov <p.todorov1015@gmail.com>
2024-01-06 21:05:43 +0200

    initial commit

---
+++ Convert A Hex String To RGB.py
@@ -0,0 +1,9 @@
+def hex_string_to_RGB(hex_string):
+    r = int(hex_string[1:3], 16)
+    g = int(hex_string[3:5], 16)
+    b = int(hex_string[5:], 16)
+
+    return {'r': r, 'g': g, 'b': b}
+
+
+print(hex_string_to_RGB("#FF9933"))
